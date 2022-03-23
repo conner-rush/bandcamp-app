76f0990f695eab9168d34603e75d7ed6d8e76820
Conner <cwr00001@mix.wvu.edu>
2022-03-23 19:44:51 -0400

    test 2

---
+++ src/app/cards/sidecards/story.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ProductItemModel } from "./product-item-model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StoryService {
+
+    private baseUrl: string = 'https://bandcamp-project-f1a1f-default-rtdb.firebaseio.com/';
+    private storiesEndPoint: string = 'stroies.json';
+
+    constructor(private http:HttpClient) {
+        
+    }
+
+public getStories()
+{
+    return this.http.get<ProductItemModel []>(this.baseUrl + this.storiesEndPoint);
+}
+
+public getStory(index: number)
+{
+    return this.http.get<ProductItemModel>(this.baseUrl + "stories/" + index + ".json");
+
+}
+}
